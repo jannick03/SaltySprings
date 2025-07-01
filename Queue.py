e359efcca568cc333dc9324662cb575de34871ea
Johannes <1107jjs@web.de>
2025-07-01 22:50:15 +0200

    change back queue

--- Queue.py
+++ Queue.py
@@ -1,16 +1,16 @@
-from components import component
-
+from box import box
+from typing import List
 
 class queue:
-    def __init__(self):
-        self.items = []
+    def __init__(self, box: List[box]):
+        self.boxes = box
 
-    def enqueue(self, comp: component):
+    def enqueue(self, box: box):
         """Add an item to the end of the queue."""
-        self.items.append(comp)
+        self.boxes.append(box)
 
-    def dequeue(self, comp: component) -> None:
+    def dequeue(self, box: box) -> None:
         """Remove a specific item from the queue."""
-        if not len(self.items) > 0:
-            self.items.remove(comp)
-        raise ValueError("Component not found in queue")
+        if not len(self.boxes) > 0:
+            self.boxes.remove(box)
+        raise ValueError("box not found in queue")

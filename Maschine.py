ad9c2c46c4ecdd9f068e3d83ae8cfd30f9f2085d
benbinkert <137608997+benbinkert@users.noreply.github.com>
2025-07-01 22:11:10 +0200

    D:

--- Maschine.py
+++ Maschine.py
@@ -1,9 +1,21 @@
 class Maschine:
     def __init__(self, id: int, name: str, funktion: str, status: str = "offline"):
-        self.id = id
-        self.name = name
-        self.funktion = funktion
+        self._id = id
+        self._name = name
+        self._funktion = funktion
         self.status = status
+
+    @property
+    def id(self) -> int:
+        return self._id
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def funktion(self) -> str:
+        return self._funktion
 
     def set_status(self, status: str):
         self.status = status

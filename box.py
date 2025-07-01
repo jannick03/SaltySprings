fb8988223281618aef64367f7b78278b052d94e1
Andi E <andreas.esslinger03@gmail.com>
2025-07-01 22:20:45 +0200

    3x Sex

--- box.py
+++ box.py
@@ -1,13 +1,13 @@
-class steckbrief:
+class box:
     counter = 1
 
     def __init__(self, items):
-        self.id = steckbrief.counter
-        steckbrief.counter += 1
+        self.id = box.counter
+        box.counter += 1
         self.items = items
 
     def __str__(self):
-        result = f"Steckbrief ID: {self.id}\n"
+        result = f"Box ID: {self.id}\n"
         result += "Erkannte Objekte:\n"
         for class_id, name in self.items:
             result += f"ID: {class_id}, Name: {name}\n"

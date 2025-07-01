c236ba127fd49a98a9b9cbebdd182dc40cc0f1d7
Johannes <1107jjs@web.de>
2025-07-01 22:17:09 +0200

    changed import

--- Queue.py
+++ Queue.py
@@ -1,15 +1,15 @@
-import Component
+from components import component
 
 
 class Queue:
     def __init__(self):
-        self.items: list[Component.Component] = []
+        self.items: list[component.Component] = []
     
-    def enqueue(self, item: Component.Component):
+    def enqueue(self, item: component.Component):
         """Add an item to the end of the queue."""
         self.items.append(item)
 
-    def dequeue(self, component: Component.Component) -> None:
+    def dequeue(self, component: component.Component) -> None:
         """Remove a specific item from the queue."""
         if not len(self.items) > 0:
             self.items.remove(component)

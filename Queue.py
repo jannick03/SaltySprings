9eb86dc1afa3f8d4398896aeba99d4bfd19db916
Johannes <1107jjs@web.de>
2025-07-01 22:38:09 +0200

    name changed to boxes

--- Queue.py
+++ Queue.py
@@ -3,15 +3,15 @@ from box import box
 
 class Queue:
     def __init__(self):
-        self.items: list[box] = []
+        self.boxes: list[box] = []
     
     def enqueue(self, item: box) -> None:
         """Add an item to the end of the queue."""
-        self.items.append(item)
+        self.boxes.append(item)
 
     def dequeue(self, component: box) -> None:
         """Remove a specific item from the queue."""
-        if not len(self.items) > 0:
-            self.items.remove(component)
+        if not len(self.boxes) > 0:
+            self.boxes.remove(component)
         raise ValueError("Component not found in queue")
     

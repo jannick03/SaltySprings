1b86678d28370662c7175918b553cc3ed4e85890
Johannes <1107jjs@web.de>
2025-07-02 09:34:18 +0200

    next production step added

--- product.py
+++ product.py
@@ -14,5 +14,21 @@ class product:
         product.product_id += 1
         self.production_steps = steps
         self.components = components
-        self.current_step = self.production_steps[0]
+        self.current_step = 0
         self.nessecary_components_for_step = nessecary_components_for_step
+
+
+    def next_production_step(self) -> 'production_step':
+        """Returns the next production step."""
+        if self.current_step is not None:
+            current_index = self.production_steps.index(self.current_step)
+            if current_index + 1 < len(self.production_steps):
+                self.current_step += 1
+                return self.production_steps[current_index + 1]
+        return None
+    
+    def get_current_step(self) -> 'production_step':
+        """Returns the current production step."""
+        if self.current_step is not None:
+            return self.production_steps[self.current_step]
+        return None

d484e5c8c060ae277d9a0e904687d188b092c22d
Andi E <andreas.esslinger03@gmail.com>
2025-07-02 11:29:58 +0200

    order

--- order.py
+++ order.py
@@ -1,9 +1,9 @@
 from typing import List
 
-from product import product
+import product
 
 
 class order:
     def __init__(self, products, product_no):
-        self.products: List['product'] = products
+        self.products: List['product.product'] = products
         self.product_no: List[int] = product_no

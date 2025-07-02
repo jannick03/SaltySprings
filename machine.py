c7eaa1d76163f2ebd86849b598049ddbfd3fbb91
benbinkert <137608997+benbinkert@users.noreply.github.com>
2025-07-02 08:40:35 +0200

    D:

--- machine.py
+++ machine.py
@@ -1,3 +1,5 @@
+from product import product
+
 class machine:
     def __init__(self, id: int, name: str, funktion: str, status: str = "offline"):
         self.id = id
@@ -11,8 +13,12 @@ class machine:
             raise ValueError("Status must be 'offline' or 'online'.")
         self.status = status
 
-    def execute_function(self):
-        return f"Maschine {self.id} ({self.name}) is performing its function: {self.funktion}"
+    def execute_function(self, product: 'product'):
+        if product.production_steps:
+            next_step = product.production_steps.pop(0)  # Remove the first step
+            return f"Maschine {self.id} ({self.name}) is performing step '{next_step}' for product '{product.name}'."
+        else:
+            return f"Maschine {self.id} ({self.name}) has completed all steps for product '{product.name}'."
 
     def get_info(self) -> str:
         return f"Maschine ID: {self.id}, Name: {self.name}, Funktion: {self.funktion}, Status: {self.status}"

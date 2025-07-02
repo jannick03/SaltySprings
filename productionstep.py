7b3b6ef4f179d2d6da153276238b4c1ae6bfa33b
Andi E <andreas.esslinger03@gmail.com>
2025-07-02 08:34:13 +0200

    Kinda sexy Bilderkennung

--- productionstep.py
+++ productionstep.py
@@ -1,12 +1,12 @@
-from Maschine import Maschine
+from machine import machine
 
 class production_step:
     step_id = 1
 
     def __init__(self, name: str, description: str, workstation: Maschine):
         self.name = name
-        self.id = production_steps.step_id
-        production_steps.step_id += 1
+        self.id = production_step.step_id
+        production_step.step_id += 1
         self.description = description
         self.workstation = workstation
 
